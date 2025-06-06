82019995b2bc8859342ca342aa0803fa5fc3b7e6
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-06 09:59:01 +0200

    Initial commit

---
+++ src/main/java/com/grajilla/aviberico/AvibericoApplication.java
@@ -0,0 +1,13 @@
+package com.grajilla.aviberico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvibericoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvibericoApplication.class, args);
+	}
+
+}
