822e00e3dfc1c65770a50d85a9bb72d583de8866
La Grajilla <201102038+dolmodev@users.noreply.github.com>
2025-06-06 12:17:58 +0200

    renombrar clase principal a Main

--- src/main/java/com/grajilla/aviberico/Main.java
+++ src/main/java/com/grajilla/aviberico/Main.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvibericoApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AvibericoApplication.class, args);
+		
+		SpringApplication.run(Main.class, args);
+		
 	}
-
 }
