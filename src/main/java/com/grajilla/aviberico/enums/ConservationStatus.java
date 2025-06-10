65f3505822d9bee802d9581362a1900e32a904b9
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-10 11:48:02 +0200

    crear enumeración ConservationStatus

---
+++ src/main/java/com/grajilla/aviberico/enums/ConservationStatus.java
@@ -0,0 +1,12 @@
+package com.grajilla.aviberico.enums;
+
+public enum ConservationStatus {
+    EXTINCT,
+    EXTINCT_IN_THE_WILD,
+    CRITICALLY_ENDANGERED,
+    ENDANGERED,
+    VULNERABLE,
+    NEAR_THREATENED,
+    CONSERVATION_DEPENDENT,
+    LEAST_CONCERN;
+}
