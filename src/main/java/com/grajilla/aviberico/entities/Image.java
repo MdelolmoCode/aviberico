2b07802ab088550a90847b8e2a022d16bd9fce7a
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-10 10:28:19 +0200

    crear entidad Image

---
+++ src/main/java/com/grajilla/aviberico/entities/Image.java
@@ -0,0 +1,26 @@
+package com.grajilla.aviberico.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imgUrl;
+
+    private String altText;
+
+    private String photographer;
+}
