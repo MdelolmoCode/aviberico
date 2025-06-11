4c39707c8437519feb9666d0565502ccb52c8c4a
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-11 09:47:06 +0200

    crear entidad Pajarero

---
+++ src/main/java/com/grajilla/aviberico/entities/Pajarero.java
@@ -0,0 +1,49 @@
+package com.grajilla.aviberico.entities;
+
+import com.grajilla.aviberico.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(exclude = {"favoriteBird"})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "pajareros")
+public class Pajarero { // entidad usuario
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // email único para el login, no puede ser null
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    // muchos usuarios puede tener la misma ave favorita, pero sólo UNA
+    @ManyToOne
+    @JoinColumn(name = "favorite_bird_id")
+    private Bird favoriteBird;
+
+    // por defecto todos los usuarios están activos
+    @Builder.Default
+    private Boolean isActive = true;
+
+    // guardo el rol como texto
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    // por defecto todos los usuarios son USER, no ADMIN
+    @Builder.Default
+    private UserRole role = UserRole.USER;
+}
