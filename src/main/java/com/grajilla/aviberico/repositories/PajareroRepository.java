80d9c05076794e0c5e4da5a2d61c7c09966b04c4
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-11 12:22:17 +0200

    crear PajareroRepository

---
+++ src/main/java/com/grajilla/aviberico/repositories/PajareroRepository.java
@@ -0,0 +1,7 @@
+package com.grajilla.aviberico.repositories;
+
+import com.grajilla.aviberico.entities.Pajarero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PajareroRepository extends JpaRepository<Pajarero, Long> {
+}
