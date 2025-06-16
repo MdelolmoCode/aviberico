8cf1e4fb590c4ab89517fe98feabd19f2956a760
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-16 13:30:39 +0200

    crear método findByFamily

--- src/main/java/com/grajilla/aviberico/controllers/BirdController.java
+++ src/main/java/com/grajilla/aviberico/controllers/BirdController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -26,6 +27,23 @@ public class BirdController {
 
         // pasar datos a la vista
         model.addAttribute("birds", birds);
+
+        return "bird/bird-list";
+    }
+
+    @GetMapping("/birds/familia/{familyName}")
+    public String findByFamily(Model model, @PathVariable String familyName) {
+        log.info("Filtrar aves por familia: {}", familyName);
+
+        // buscar aves de la familia específica
+        List<Bird> birds = birdRepository.findByFamily(familyName);
+        // encontrar lista de familias
+        List<String> familias = birdRepository.findDistinctFamilies();
+
+        // pasar resultados de filtrado a vista
+        model.addAttribute("birds", birds);
+        model.addAttribute("familias", familias);
+        model.addAttribute("familiaFiltrada", familyName);
 
         return "bird/bird-list";
     }
