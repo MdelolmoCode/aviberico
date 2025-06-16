96446fc85ade42e677f9600a45d028f340969c18
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-16 11:45:54 +0200

    añadir displayName a la enumeración ConservationStatus

--- src/main/java/com/grajilla/aviberico/enums/ConservationStatus.java
+++ src/main/java/com/grajilla/aviberico/enums/ConservationStatus.java
@@ -1,12 +1,22 @@
 package com.grajilla.aviberico.enums;
 
 public enum ConservationStatus {
-    EXTINCT,
-    EXTINCT_IN_THE_WILD,
-    CRITICALLY_ENDANGERED,
-    ENDANGERED,
-    VULNERABLE,
-    NEAR_THREATENED,
-    CONSERVATION_DEPENDENT,
-    LEAST_CONCERN;
+    EXTINCT("Extinto"),
+    EXTINCT_IN_THE_WILD("Extinto en estado silvestre"),
+    CRITICALLY_ENDANGERED("En peligro crítico"),
+    ENDANGERED("En peligro"),
+    VULNERABLE("Vulnerable"),
+    NEAR_THREATENED("Casi amenazado"),
+    CONSERVATION_DEPENDENT("Dependiente de conservación"),
+    LEAST_CONCERN("Preocupación menor");
+
+    private final String displayName;
+
+    ConservationStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
