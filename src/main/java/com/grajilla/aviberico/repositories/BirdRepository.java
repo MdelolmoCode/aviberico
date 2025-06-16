508c11b33edd04f67d3ccc24e383c6cee7e2ce1d
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-16 12:31:24 +0200

    crear método y jquery para filtrar por familia

--- src/main/java/com/grajilla/aviberico/repositories/BirdRepository.java
+++ src/main/java/com/grajilla/aviberico/repositories/BirdRepository.java
@@ -2,6 +2,16 @@ package com.grajilla.aviberico.repositories;
 
 import com.grajilla.aviberico.entities.Bird;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BirdRepository extends JpaRepository<Bird, Long> {
+
+    // buscar por familia
+    List<Bird> findByFamily(String family);
+
+    // obtener familias únicas
+    @Query("SELECT DISTINCT b.family FROM Bird b WHERE b.family IS NOT NULL ORDER BY b.family")
+    List<Bird> findDistincFamilies();
 }
