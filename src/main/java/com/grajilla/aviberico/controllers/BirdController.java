25f249c68c7ab94b99a05cb90047236b072eccd7
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-17 09:35:00 +0200

    crear método para buscar aves por nombre

--- src/main/java/com/grajilla/aviberico/controllers/BirdController.java
+++ src/main/java/com/grajilla/aviberico/controllers/BirdController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,6 +30,21 @@ public class BirdController {
         // pasar datos a la vista
         model.addAttribute("birds", birds);
         model.addAttribute("familias", familias);
+
+        return "bird/bird-list";
+    }
+
+    @GetMapping("/birds/buscar")
+    public String findByCommonName(Model model, @RequestParam String nombre) {
+        // buscar aves por nombre
+        List<Bird> birds = birdRepository.findByCommonNameContainsIgnoreCase(nombre);
+        // encontrar lista de familias
+        List<String> familias = birdRepository.findDistinctFamilies();
+
+        // pasar resultado de filtrado a la vista
+        model.addAttribute("birds", birds);
+        model.addAttribute("familias", familias);
+        model.addAttribute("busquedaActual", nombre);
 
         return "bird/bird-list";
     }
