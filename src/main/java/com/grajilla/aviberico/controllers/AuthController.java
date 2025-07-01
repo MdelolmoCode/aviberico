2d67376d0c73cc6e62469fb240e98ae76343c52d
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-07-01 09:45:21 +0200

    pantalla login

---
+++ src/main/java/com/grajilla/aviberico/controllers/AuthController.java
@@ -0,0 +1,13 @@
+package com.grajilla.aviberico.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
